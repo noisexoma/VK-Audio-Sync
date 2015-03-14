2a3458ae8b34f96e7f8a788ef43af7c01bd107f1
Roman <RomzesRover@gmail.com>
2015-03-15 00:08:13 +0500

    delete bin and gen

--- MainResourcesCollector/gen/com/BBsRs/vkaudiosync/resources/collector/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.BBsRs.vkaudiosync.resources.collector;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
