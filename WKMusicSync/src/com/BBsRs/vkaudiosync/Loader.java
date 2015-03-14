40ec58271ccd2bccb082c7729b2dd87f0065ddb0
Roman <RomzesRover@gmail.com>
2015-03-15 00:06:04 +0500

    init commit of version two

---
+++ WKMusicSync/src/com/BBsRs/vkaudiosync/Loader.java
@@ -0,0 +1,14 @@
+package com.BBsRs.vkaudiosync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Loader extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loader);
+    }
+}
